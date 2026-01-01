5b2b52d7be13205d9b074ba0c3256b5b8a32a659
Yoshiyuki Okamoto <okamoto53515606@gmail.com>
2026-01-01 05:21:51 +0000

    ありがとう。ヘッダーのアクセス有効期限は有料会員期限に変更願います。

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -25,7 +25,7 @@ function UserStatus({ user }: { user: Awaited<ReturnType<typeof getUser>> }) {
     const expiryDate = new Date(user.accessExpiry).toLocaleDateString('ja-JP');
     return (
       <div className="header__center">
-        <span>アクセス有効期限: {expiryDate}</span>
+        <span>有料会員期限: {expiryDate}</span>
       </div>
     );
   }
